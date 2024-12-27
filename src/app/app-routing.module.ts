c7809584c0335c29107a2d428957e09035ddff57
Rafel <rafael_estarellas@yahoo.es>
2024-12-27 19:51:45 +0100

    cream key de sa api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 // filepath: /c:/Users/Jacky/Desktop/git-copilot/curs_git_copilot/tiempo-app/src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherComponent } from './pages/weather/weather.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { MapComponent } from './pages/map/map.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'weather', component: WeatherComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'map', component: MapComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
 const routes: Routes = [
   // Aquí puedes agregar tus rutas
 ];
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private apiKey = '4d1739fdc00f4984dea50614dbee5417';
+  constructor(private http: HttpClient) { }
+  getWeather(city: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}?q=${city}&appid=${this.apiKey}`);
+  }
+}
