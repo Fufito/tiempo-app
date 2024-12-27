3499a1bb62a42e04db9e26f9bc6e9c65ea03ad39
Rafel <rafael_estarellas@yahoo.es>
2024-12-27 19:14:05 +0100

    afegiir fitxer d'enrutament

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+// filepath: /c:/Users/Jacky/Desktop/git-copilot/curs_git_copilot/tiempo-app/src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Aquí puedes agregar tus rutas
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
