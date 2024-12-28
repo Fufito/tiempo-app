e9affddbed7310b564afa6f09381a5f17ea28150
Rafel <rafael_estarellas@yahoo.es>
2024-12-28 20:04:57 +0100

    darrrers intents

---
+++ src/app/components/map-component/map.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  private map: L.Map | undefined;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    L.marker([51.505, -0.09]).addTo(this.map)
+      .bindPopup('Default Location')
+      .openPopup();
+  }
+}
